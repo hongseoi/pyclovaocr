1e21f504af1932d2f059414ce63a65c76f272d70
kdrkdrkdr <kdrhacker1234@gmail.com>
2022-04-30 18:20:56 +0900

    add files

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="pyclovaocr",
+    version="1.0",
+    license='MIT',
+    author="kdr",
+    author_email="kdrhacker1234@gmail.com",
+    description="Unofficial Clova AI OCR python wrapper",
+    long_description=open('README.md', encoding='utf-8').read(),
+    url="https://github.com/kdrkdrkdr/pyclovaocr",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=['requests'],
+    py_modules = ['pyclovaocr']
+)
